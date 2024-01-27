ce4e0cf75a1690705ec82b1fda80aec591b100f8
Rushikesh Here <rushikeshhere@gmail.com>
2024-01-27 15:29:14 +0530

    Insertion in a linkedlist

---
+++ src/com/practice/Day06/Main.java
@@ -0,0 +1,19 @@
+package com.practice.Day06;
+
+import java.util.LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//create Obj of linkedList
+		LinkedList<String>list=new LinkedList<>();
+		list.add("Java");
+		list.add("Python");
+		list.add("C++");
+		list.add("Ruby");
+		System.out.println("Programming languages are :: "+list);
+
+
+	}
+
+}
