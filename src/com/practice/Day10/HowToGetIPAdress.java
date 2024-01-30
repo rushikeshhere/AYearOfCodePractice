8e0be40d83272d823f14aba87a9191eafba1d7eb
Rushikesh Here <rushikeshhere@gmail.com>
2024-01-30 20:58:41 +0530

    How to get own IP Address

---
+++ src/com/practice/Day10/HowToGetIPAdress.java
@@ -0,0 +1,10 @@
+package com.practice.Day10;
+
+import java.net.InetAddress;
+
+public class HowToGetIPAdress {
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println(InetAddress.getLocalHost());
+	}
+}
