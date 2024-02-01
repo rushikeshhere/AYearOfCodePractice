e74176d85e60fd113e554a3423e167a9611c1d0c
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-01 21:47:00 +0530

    converted string to long

---
+++ src/com/practice/Day12/StringToLong.java
@@ -0,0 +1,11 @@
+package com.practice.Day12;
+
+public class StringToLong {
+
+	public static void main(String[] args) {
+		String number="9874561236";
+		long l=Long.parseLong(number);
+		System.out.println(l);
+	}
+
+}
