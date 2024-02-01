f7758d26818a90ef3dd8668cd986cacacd0f28b6
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-01 21:49:54 +0530

    converted string to double

---
+++ src/com/practice/Day12/StringToDouble.java
@@ -0,0 +1,10 @@
+package com.practice.Day12;
+
+public class StringToDouble {
+	public static void main(String[] args) {
+		
+		String s="23.6";
+		double d=Double.parseDouble(s);
+		System.out.println(d+1.5);
+	}
+}
