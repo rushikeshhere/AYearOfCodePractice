06b0fd770ed1cb28cf9700569c046e34146ca471
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-01 21:44:30 +0530

    converted integer to string

---
+++ src/com/practice/Day12/ConvertingIntgerToString.java
@@ -0,0 +1,11 @@
+package com.practice.Day12;
+
+public class ConvertingIntgerToString {
+	public static void main(String[] args) {
+		int i=230;
+		String number=String.valueOf(i);
+		System.out.println(number+100);
+		System.out.println(i+200);
+	}
+
+}
