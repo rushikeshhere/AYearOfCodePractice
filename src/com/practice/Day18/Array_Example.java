7b13f9b9de2bae54c103b356390c4ef80804ac7c
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-07 12:01:08 +0530

    Array Example

---
+++ src/com/practice/Day18/Array_Example.java
@@ -0,0 +1,19 @@
+package com.practice.Day18;
+
+public class Array_Example {
+	public static void main(String[] args) {
+		
+		int [] a=new int[5];
+		a[0]=10;
+		a[1]=15;
+		a[2]=20;
+		a[3]=25;
+		a[4]=30;
+		for(int i=0;i<a.length;i++) {
+			System.out.print(a[i]+" ");
+		}
+		
+		
+	}
+
+}
