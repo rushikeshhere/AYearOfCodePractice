dc6a31fd65a429c5c0decb25da4da02dde3e1414
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-09 10:49:43 +0530

    Used LocalDateTime API to print the Current Date And Time

---
+++ src/com/practice/Day20/LocalTimeDateAPI.java
@@ -0,0 +1,11 @@
+package com.practice.Day20;
+
+import java.time.LocalDateTime;
+
+public class LocalTimeDateAPI {
+	
+	public static void main(String[] args) {
+		LocalDateTime dateTime=LocalDateTime.now();
+			System.out.println(dateTime); //2024-02-09T10:46:05.307242800
+		}
+}
