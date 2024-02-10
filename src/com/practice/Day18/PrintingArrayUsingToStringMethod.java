c0c2dd53604b049089ff9e79c0ac99d663c42737
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-10 07:28:50 +0530

    Printing Array using Arrray.toString() method

---
+++ src/com/practice/Day18/PrintingArrayUsingToStringMethod.java
@@ -0,0 +1,11 @@
+package com.practice.Day18;
+
+import java.util.Arrays;
+
+public class PrintingArrayUsingToStringMethod {
+	public static void main(String[] args) {
+		
+		int [] array= {10,20,30,45,60,70};
+		System.out.println(Arrays.toString(array));//[10, 20, 30, 45, 60, 70]
+	}
+}
