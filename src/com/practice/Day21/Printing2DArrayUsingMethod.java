e834c97da0112fd77861102a891a9889570ea943
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-10 11:59:24 +0530

    printing 2D Array using pre-defined method

---
+++ src/com/practice/Day21/Printing2DArrayUsingMethod.java
@@ -0,0 +1,13 @@
+package com.practice.Day21;
+
+import java.util.Arrays;
+
+public class Printing2DArrayUsingMethod {
+	public static void main(String[] args) {
+		// declare and initialize an array
+		int array[][] = { { 10, 20 }, { 30, 40 }, { 50, 60 } };
+		// display 2D array using Arrays.deepToString()
+		System.out.println(Arrays.deepToString(array));// [[10, 20], [30, 40], [50, 60]]
+
+	}
+}
