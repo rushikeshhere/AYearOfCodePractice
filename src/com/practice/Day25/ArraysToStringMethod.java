0ba082abf7434eb3c8c495086d07867cdf0a5434
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-14 11:25:48 +0530

    Checked how toString method is working

---
+++ src/com/practice/Day25/ArraysToStringMethod.java
@@ -0,0 +1,13 @@
+package com.practice.Day25;
+
+import java.util.Arrays;
+
+public class ArraysToStringMethod {
+	public static void main(String[] args) {
+		int array[] = {};
+		System.out.println(Arrays.toString(array));
+		int array1[] = null;
+		System.out.println(Arrays.toString(array1));
+	}
+
+}
