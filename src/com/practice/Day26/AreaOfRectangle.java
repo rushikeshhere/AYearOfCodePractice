ff93aa855139a720966a654a76a2ca99d7966848
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-15 10:20:01 +0530

    Calculating

---
+++ src/com/practice/Day26/AreaOfRectangle.java
@@ -0,0 +1,11 @@
+package com.practice.Day26;
+
+public class AreaOfRectangle {
+	public static void main(String[] args) {
+		double length = 5;
+		double width = 2.25;
+		double area = 0.0;
+		area = length * width;
+		System.out.println("Area of Rectangle is :: " + area);
+	}
+}
