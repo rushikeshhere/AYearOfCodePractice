4328c7b51117d828d1fad7aa5ca6f0f2e50e43c7
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-16 12:38:45 +0530

    By StringBuilder / StringBuffer

---
+++ src/com/practice/Day27/ReversingString.java
@@ -0,0 +1,10 @@
+package com.practice.Day27;
+
+public class ReversingString {
+	public static void main(String[] args) {
+		String str="Here";
+		StringBuilder sbi=new StringBuilder(str);
+		sbi.reverse();
+		System.out.println(sbi);
+	}
+}
