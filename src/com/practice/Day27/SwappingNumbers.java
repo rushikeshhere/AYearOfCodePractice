02565e73f799f061d79f472877743e1159f537e3
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-16 12:25:57 +0530

    Swap two numbers using Bitwise XOR operator (^)

---
+++ src/com/practice/Day27/SwappingNumbers.java
@@ -0,0 +1,16 @@
+package com.practice.Day27;
+
+public class SwappingNumbers {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 15;
+		System.out.println("Before Swapping :: x=" + x + "\t y=" + y);
+		x = x ^ y;
+		y = x ^ y;
+		x = x ^ y;
+
+		
+		System.out.println("After Swapping :: x=" + x + "\t y=" + y);
+	}
+
+}
