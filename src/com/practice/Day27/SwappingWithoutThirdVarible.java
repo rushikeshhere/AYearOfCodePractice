f64aab1f7abae9118167d307ae185ee092ee1b36
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-16 12:15:36 +0530

    Swapping without third variable

---
+++ src/com/practice/Day27/SwappingWithoutThirdVarible.java
@@ -0,0 +1,14 @@
+package com.practice.Day27;
+
+public class SwappingWithoutThirdVarible {
+	public static void main(String[] args) {
+		
+		int x=10;
+		int y=15;
+		System.out.println("Before Swapping :: x=" + x + "\t y=" + y);
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		System.out.println("Before Swapping :: x=" + x + "\t y=" + y);
+	}
+}
