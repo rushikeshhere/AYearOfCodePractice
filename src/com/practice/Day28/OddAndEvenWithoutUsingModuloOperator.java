4a9e7f2b94fe1306fbb2910d08b89e89966402c5
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-17 18:01:07 +0530

    Even or Odd Program without using Modulus Operator(%)

--- src/com/practice/Day28/OddAndEvenWithoutUsingModuloOperator.java
+++ src/com/practice/Day28/OddAndEvenWithoutUsingModuloOperator.java
@@ -2,13 +2,13 @@ package com.practice.Day28;
 
 import java.util.Scanner;
 
-public class OddEvenUsingTernaryOperator {
+public class OddAndEvenWithoutUsingModuloOperator {
 	public static boolean oddAndEven(int num) {
-		if ((num / 2) * 2==num)
+		if (num % 2 == 0)
 			return true;
-		return false;
+		else
+			return false;
 	}
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter any number to check Even or Odd..");
