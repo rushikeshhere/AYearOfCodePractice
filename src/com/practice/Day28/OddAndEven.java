bf62ea9a0f9ab60c6db9363cf3dd5a437d4136bc
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-17 17:37:38 +0530

    even-odd program

---
+++ src/com/practice/Day28/OddAndEven.java
@@ -0,0 +1,21 @@
+package com.practice.Day28;
+
+import java.util.Scanner;
+
+public class OddAndEven {
+	public static boolean oddAndEven(int num) {
+		if (num % 2 == 0)
+			return true;
+		else
+			return false;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number to check Even or Odd..");
+		int number = sc.nextInt();
+
+		boolean result = oddAndEven(number);
+		System.out.println(result);
+		sc.close();
+	}
+}
