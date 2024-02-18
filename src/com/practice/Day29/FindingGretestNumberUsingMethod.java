75780a2aeb0a6c46007e1fbf6e94e6f6bf07a186
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-18 08:02:07 +0530

    Largest of three numbers using user defined method

---
+++ src/com/practice/Day29/FindingGretestNumberUsingMethod.java
@@ -0,0 +1,21 @@
+package com.practice.Day29;
+
+public class FindingGretestNumberUsingMethod {
+	public static double find(int a, int b, int c) {
+		if (a >= b) {
+			if (a >= c) {
+				return a;
+			} else
+				return c;
+		} else if (b >= c) {
+			return b;
+		} else
+			return c;
+	}
+
+	public static void main(String[] args) {
+		double result = find(15, 25, 48);
+
+		System.out.println("The Largest Number Among Three Numbers is :: " + result);
+	}
+}
