4a8d3d8f40e94f43a3ed06196b97d7990ef6d256
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-19 15:02:38 +0530

    Multiplication table using for Loop

---
+++ src/com/practice/Day30/MultiplicationTable.java
@@ -0,0 +1,15 @@
+package com.practice.Day30;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int num = sc.nextInt();
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num + " * " + i + " = " + num * i);
+		}
+		sc.close();
+	}
+}
