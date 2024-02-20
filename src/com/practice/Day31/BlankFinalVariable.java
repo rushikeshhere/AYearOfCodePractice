83b835ac9bf9fe511310c0c352738b646934d4cf
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-20 13:52:05 +0530

    What is the blank final variable

---
+++ src/com/practice/Day31/BlankFinalVariable.java
@@ -0,0 +1,11 @@
+package com.practice.Day31;
+
+public class BlankFinalVariable {
+	public static void main(String[] args) {
+
+		final int num;// Blank final variables those will not initiate during declaration..
+		num = 10;
+		System.out.println(num);
+	
+	}
+}
