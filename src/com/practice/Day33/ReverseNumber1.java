2cc47e8e5e723c1030025d204424d5a1e72cc2d6
Rushikesh Here <rushikeshhere@gmail.com>
2024-02-22 11:22:13 +0530

    Reversed a number using StringBuilder.

---
+++ src/com/practice/Day33/ReverseNumber1.java
@@ -0,0 +1,11 @@
+package com.practice.Day33;
+
+public class ReverseNumber1 {
+	public static void main(String[] args) {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(123);
+		sb.reverse();
+		System.out.println(sb);
+	}
+}
