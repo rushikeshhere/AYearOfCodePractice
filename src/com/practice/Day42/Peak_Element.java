d3512c93e7fc66bd01ea60582d90e86dbbdf68a6
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-02 15:00:49 +0530

    Finding Peak Element of an array

---
+++ src/com/practice/Day42/Peak_Element.java
@@ -0,0 +1,11 @@
+package com.practice.Day42;
+
+public class Peak_Element {
+	public static void main(String[] args) {
+		int array[]= {1,2,3};
+		for(int a:array) {
+			System.out.print(a+" ");
+		}
+	}
+
+}
