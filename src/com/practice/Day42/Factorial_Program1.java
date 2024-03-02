fe6af59d3b819d8fef76d9a7c5302ab32d5140c2
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-02 19:44:35 +0530

    Factorial program2

---
+++ src/com/practice/Day42/Factorial_Program1.java
@@ -0,0 +1,18 @@
+package com.practice.Day42;
+
+import java.util.Scanner;
+
+public class Factorial_Program1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number :: ");
+		int number = sc.nextInt();
+		int fact = 1;
+		for (int i = number; i > 1; i--) {
+			fact = i * fact;
+		}
+		System.out.println(fact);
+		sc.close();
+	}
+
+}
