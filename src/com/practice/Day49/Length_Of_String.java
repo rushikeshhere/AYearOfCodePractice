9749db0c70fef23a322ddd37523242db410faeee
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-09 21:33:18 +0530

    How to Find Length of String in Java

---
+++ src/com/practice/Day49/Length_Of_String.java
@@ -0,0 +1,8 @@
+package com.practice.Day49;
+
+public class Length_Of_String {
+	public static void main(String[] args) {
+		String str = "Rushikesh";
+		System.out.println("The Length of String is :: " + str.length());
+	}
+}
