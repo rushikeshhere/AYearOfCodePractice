30c70f59646cbbe7af0bd61c807bedec567b485a
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-10 21:48:51 +0530

    Iterate Through Characters In a String

---
+++ src/com/practice/Day50/Iterating_String_UsingForLoop.java
@@ -0,0 +1,11 @@
+package com.practice.Day50;
+
+public class Iterating_String_UsingForLoop {
+	public static void main(String[] args) {
+
+		String str = "Java Programming";
+		for (int i = 0; i < str.length(); i++) {
+			System.out.print(str.charAt(i));
+		}
+	}
+}
