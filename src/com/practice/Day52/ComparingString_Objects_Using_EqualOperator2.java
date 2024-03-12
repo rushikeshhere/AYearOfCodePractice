f54f3e941e5dc540e65e8fde6e0bc84ae4ed3d74
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-12 17:44:57 +0530

    compared two Strings Objects using == operator

---
+++ src/com/practice/Day52/ComparingString_Objects_Using_EqualOperator2.java
@@ -0,0 +1,9 @@
+package com.practice.Day52;
+
+public class ComparingString_Objects_Using_EqualOperator2 {
+	public static void main(String[] args) {
+		String str1 = new String("Hello");
+		String str2 = new String("Hello");
+		System.out.println(str1 == str2);
+	}
+}
