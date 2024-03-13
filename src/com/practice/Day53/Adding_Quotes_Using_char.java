d84985451851658d893dbd12f5948a8803ddfdf3
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-13 22:05:10 +0530

    Java Double Quotes In String using char

---
+++ src/com/practice/Day53/Adding_Quotes_Using_char.java
@@ -0,0 +1,9 @@
+package com.practice.Day53;
+
+public class Adding_Quotes_Using_char {
+	public static void main(String[] args) {
+		char doubleQuote = '"';
+		String str = "Java Programming";
+		System.out.println(doubleQuote + str + doubleQuote);
+	}
+}
