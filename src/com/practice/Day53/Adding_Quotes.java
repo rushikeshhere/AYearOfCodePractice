af8b7e34508a41dc2227f80715672adea7a0839e
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-13 21:58:57 +0530

    Program to Put Quotes in a String Java

---
+++ src/com/practice/Day53/Adding_Quotes.java
@@ -0,0 +1,10 @@
+package com.practice.Day53;
+
+public class Adding_Quotes {
+	public static void main(String[] args) {
+		String str = "\"Java Programming\"";
+		System.out.println(str);
+		String str1 = "\'Java Programming\'";
+		System.out.println(str1);
+	}
+}
