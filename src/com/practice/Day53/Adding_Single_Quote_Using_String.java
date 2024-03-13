3d18e5830f2fa403aeeaf8543aa5da0ae2437c5b
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-13 22:09:24 +0530

    Java Double Quotes In String using String

---
+++ src/com/practice/Day53/Adding_Single_Quote_Using_String.java
@@ -0,0 +1,9 @@
+package com.practice.Day53;
+
+public class Adding_Single_Quote_Using_String {
+	public static void main(String[] args) {
+		String singleQuote = "'";
+		String str = "Java Programming";
+		System.out.println(singleQuote + str + singleQuote);
+	}
+}
