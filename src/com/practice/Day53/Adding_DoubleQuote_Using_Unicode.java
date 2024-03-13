f1e37e152c3e4a40aad4a549944b7143022b9c8b
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-13 22:11:53 +0530

    Double Quotes in a String in Java using Unicode Character

---
+++ src/com/practice/Day53/Adding_DoubleQuote_Using_Unicode.java
@@ -0,0 +1,8 @@
+package com.practice.Day53;
+
+public class Adding_DoubleQuote_Using_Unicode {
+	public static void main(String[] args) {
+		String str = '\u0022' + "Java Programming" + '\u0022';
+		System.out.println(str);
+	}
+}
