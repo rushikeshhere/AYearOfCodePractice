1ccec6123088c1a11112901ae80fb6b43a335e80
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-13 22:16:54 +0530

    Single Quotes in a String in Java using Unicode Character

---
+++ src/com/practice/Day53/Adding_SingleQuote_Using_Unicode.java
@@ -0,0 +1,8 @@
+package com.practice.Day53;
+
+public class Adding_SingleQuote_Using_Unicode {
+	public static void main(String[] args) {
+		String string ="\u0027" + "Java Programming Language" + "\u0027";
+		System.out.println(string);
+	}
+}
