5d88158ae1ad4b8df3fd9d7f521faaa1089abeb9
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-14 16:20:26 +0530

    Added A New Line After String

---
+++ src/com/practice/Day54/Adding_New_Line_After_String.java
@@ -0,0 +1,9 @@
+package com.practice.Day54;
+
+public class Adding_New_Line_After_String {
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		System.out.println("To");
+		System.out.println("Java - World");
+	}
+}
