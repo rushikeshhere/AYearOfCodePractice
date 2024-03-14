0709afa686476a4f4170b1c97a02cd84c56f4958
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-14 16:18:10 +0530

    Added new Line Using %n

---
+++ src/com/practice/Day54/Adding_New_Line1.java
@@ -0,0 +1,12 @@
+package com.practice.Day54;
+
+public class Adding_New_Line1 {
+	public static void main(String[] args) {
+		System.out.printf("Java%nProgram");
+		System.out.println();
+		String newLine = "%n";
+		System.out.printf("Hi" + newLine + "Welcome");
+		System.out.println();
+		System.out.printf("%s%n%s", "Hello", "World");
+	}
+}
