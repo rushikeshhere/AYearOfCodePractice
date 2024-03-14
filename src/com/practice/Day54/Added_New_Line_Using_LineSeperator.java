34f9de2bc5d0e7c1b2f6b36052ded2bef04bb8ea
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-14 16:08:59 +0530

    Added new Line in String using LineSeperator

---
+++ src/com/practice/Day54/Added_New_Line_Using_LineSeperator.java
@@ -0,0 +1,8 @@
+package com.practice.Day54;
+
+public class Added_New_Line_Using_LineSeperator {
+	public static void main(String[] args) {
+		String newLine = System.lineSeparator();
+		System.out.println("Java" + newLine + "Program");
+	}
+}
