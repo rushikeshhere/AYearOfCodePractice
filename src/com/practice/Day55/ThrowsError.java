e607f53ad143a4eccc8a3e2e3dbc460392352f66
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-15 16:46:25 +0530

    Checked String is Empty or not

---
+++ src/com/practice/Day55/ThrowsError.java
@@ -0,0 +1,12 @@
+package com.practice.Day55;
+
+public class ThrowsError {
+	public static void main(String[] args) {
+		String str = "";
+		System.out.println(str.isEmpty());
+		String str1 = "Know Java";
+		System.out.println(str1.isEmpty());
+		String str2 = null;
+		System.out.println(str2.isEmpty());
+	}
+}
