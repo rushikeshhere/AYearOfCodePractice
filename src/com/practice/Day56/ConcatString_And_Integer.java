92fa71bbeb5c30052c63d09d9adfc55113a206cc
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-16 22:11:12 +0530

    Concat Int to String in Java using + Operator

---
+++ src/com/practice/Day56/ConcatString_And_Integer.java
@@ -0,0 +1,10 @@
+package com.practice.Day56;
+
+public class ConcatString_And_Integer {
+	public static void main(String[] args) {
+		String str = "ABC";
+		int num = 123;
+		String result = str + num;
+		System.out.println(result);
+	}
+}
