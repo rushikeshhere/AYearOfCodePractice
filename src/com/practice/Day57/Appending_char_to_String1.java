97a5e6dcc733f446e791e3a8508459847b52ba62
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-17 21:29:38 +0530

    Appended char to String using .concat(Character.toString()

---
+++ src/com/practice/Day57/Appending_char_to_String1.java
@@ -0,0 +1,8 @@
+package com.practice.Day57;
+
+public class Appending_char_to_String1 {
+	public static void main(String[] args) {
+		String str="Book".concat(Character.toString('s'));
+		System.out.println(str);
+	}
+}
