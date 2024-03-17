2f2b996ddda54eb9a0dce2289e6bef1ddfff7bbf
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-17 21:20:37 +0530

    Appended char to String using character.toString();

---
+++ src/com/practice/Day57/Appending_char_to_String.java
@@ -0,0 +1,8 @@
+package com.practice.Day57;
+
+public class Appending_char_to_String {
+	public static void main(String[] args) {
+		String str="New"+Character.toString('s');
+		System.out.println(str);
+	}
+}
