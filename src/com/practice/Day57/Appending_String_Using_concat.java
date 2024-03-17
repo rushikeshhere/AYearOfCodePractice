50bef4875b7b19a7f074e44f3e1aa653bedf8972
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-17 21:17:07 +0530

    Appended an Strings using .concat()

---
+++ src/com/practice/Day57/Appending_String_Using_concat.java
@@ -0,0 +1,9 @@
+package com.practice.Day57;
+
+public class Appending_String_Using_concat {
+	public static void main(String[] args) {
+		String str="Know".concat("Program");
+		System.out.println(str);
+		
+	}
+}
