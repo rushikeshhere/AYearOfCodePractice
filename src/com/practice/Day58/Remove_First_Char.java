e4c6410059afc205be216cdf2efd6b05313ff243
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-18 21:09:25 +0530

    Removed first char of String

---
+++ src/com/practice/Day58/Remove_First_Char.java
@@ -0,0 +1,9 @@
+package com.practice.Day58;
+
+public class Remove_First_Char {
+	public static void main(String[] args) {
+		String str="Know Program";
+		System.out.print("String after removing the first character: ");
+		System.out.println(str.substring(1));
+	}
+}
