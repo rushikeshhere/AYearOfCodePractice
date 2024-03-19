8ff4e3f218779a7c87684f192c552b012ba0782a
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-19 10:57:46 +0530

    Example of ArrayList

---
+++ src/com/practice/Day59/ArrayList_Example.java
@@ -0,0 +1,16 @@
+package com.practice.Day59;
+
+import java.util.ArrayList;
+
+public class ArrayList_Example {
+	public static void main(String[] args) {
+		ArrayList l= new ArrayList();
+		l.add("Java");
+		l.add("Python");
+		l.add("c++");
+		l.add(45);
+		l.add(1);
+		l.add(null);
+		System.out.println(l);
+	}
+}
