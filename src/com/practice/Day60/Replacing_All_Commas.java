ed4009ccca5a437926ef9c0f7ca3ea1642cd9f22
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-20 21:24:55 +0530

    Replaced All the Commas into dots

---
+++ src/com/practice/Day60/Replacing_All_Commas.java
@@ -0,0 +1,8 @@
+package com.practice.Day60;
+
+public class Replacing_All_Commas {
+	public static void main(String[] args) {
+		String str = "www,knowProgram,com";
+		System.out.println(str.replaceAll(",", "."));
+	}
+}
