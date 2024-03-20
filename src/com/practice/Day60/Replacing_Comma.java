1ba59c423d1623c6fe45bc9c2dc693a17f5ecc45
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-20 21:22:41 +0530

    Replaced Comma from String and append dot

---
+++ src/com/practice/Day60/Replacing_Comma.java
@@ -0,0 +1,9 @@
+package com.practice.Day60;
+
+public class Replacing_Comma {
+	public static void main(String[] args) {
+		String str="www,google,com";
+		System.out.println(str.replace(",", "."));
+	}
+
+}
