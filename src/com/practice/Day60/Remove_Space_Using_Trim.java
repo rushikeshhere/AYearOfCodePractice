4a0e32c556aac83f315dc042f47a7e3686f15c7a
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-20 21:33:17 +0530

    Removed Space of String using Trim

---
+++ src/com/practice/Day60/Remove_Space_Using_Trim.java
@@ -0,0 +1,9 @@
+package com.practice.Day60;
+
+public class Remove_Space_Using_Trim {
+	public static void main(String[] args) {
+		String str = "    Learn Java Programming    ";
+		System.out.println("Before Trim String is :: " + str);
+		System.out.println("After Trim String is :: " + str.trim());
+	}
+}
