3a84f456962ccbd802d591403b48428b0e0a0e32
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-21 21:25:37 +0530

    Example of HashSet

---
+++ src/com/practice/Day61/HashSetExample.java
@@ -0,0 +1,15 @@
+package com.practice.Day61;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+	public static void main(String[] args) {
+		HashSet<String> hs = new HashSet<String>();
+		hs.add("Java");
+		hs.add("C++");
+		hs.add("Python");
+		hs.add("C");
+		hs.add("Python");
+		System.out.println(hs);
+	}
+}
