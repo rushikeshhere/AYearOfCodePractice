e442ec07239bc43f65701df7c3dc857b78e3a001
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-21 21:30:05 +0530

    Example of LinkedHashSet

---
+++ src/com/practice/Day61/LinkedHashSet_Example.java
@@ -0,0 +1,16 @@
+package com.practice.Day61;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSet_Example {
+	public static void main(String[] args) {
+
+		LinkedHashSet<String> ls = new LinkedHashSet<String>();
+		ls.add("C++");
+		ls.add("Java");
+		ls.add("Scala");
+		ls.add("Python");
+		ls.add("Ruby");
+		System.out.println(ls);
+	}
+}
