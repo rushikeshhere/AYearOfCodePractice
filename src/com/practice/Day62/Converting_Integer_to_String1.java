d8f63da32aef752c0233d6efd58d1e373845b118
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-22 21:51:09 +0530

    Converted Integer to String using toString method

---
+++ src/com/practice/Day62/Converting_Integer_to_String1.java
@@ -0,0 +1,9 @@
+package com.practice.Day62;
+
+public class Converting_Integer_to_String1 {
+	public static void main(String[] args) {
+		Integer num = 12345678;
+		String str = num.toString();
+		System.out.println("Converted Integer to String  :: " + str);
+	}
+}
