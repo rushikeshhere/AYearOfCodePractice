8f9ee58d731709c17b766edaad1ddc7094480af9
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-23 21:28:22 +0530

    Iterated String array using while Loop

---
+++ src/com/practice/Day63/Iterating_Array_Using_whileloop.java
@@ -0,0 +1,12 @@
+package com.practice.Day63;
+
+public class Iterating_Array_Using_whileloop {
+	public static void main(String[] args) {
+		String[] str = { "java", "C++", "C", "Python" };
+		int i = 0;
+		while (i < str.length) {
+			System.out.print(str[i]+" ");
+			i++;
+		}
+	}
+}
