0006818c33d76f631eed660c8268c40acb136790
Rushikesh Here <rushikeshhere@gmail.com>
2024-03-31 21:26:22 +0530

    Uppercase Array Elements

---
+++ src/com/practice/Day70/Upercase_Array_Elements.java
@@ -0,0 +1,10 @@
+package com.practice.Day70;
+
+public class Upercase_Array_Elements {
+	public static void main(String[] args) {
+		String[] str = { "java", "python", "c", "scala" };
+		for (int i = 0; i < str.length; i++) {
+			System.out.print(str[i].toUpperCase()+" ");
+		}
+	}
+}
