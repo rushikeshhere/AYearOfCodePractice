94c33656d200a9b94392964940498ca0650039a8
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-01 22:14:10 +0530

    found length of Array using .length method

---
+++ src/com/practice/Day71/Finding_length_Of_Array.java
@@ -0,0 +1,9 @@
+package com.practice.Day71;
+
+public class Finding_length_Of_Array {
+	public static void main(String[] args) {
+		String str[] = { "Rakesh", "Rajesh", "Shailesh", "Kalpesh", "Ranjeet" };
+		System.out.println("Length of Array is :: " + str.length);
+
+	}
+}
