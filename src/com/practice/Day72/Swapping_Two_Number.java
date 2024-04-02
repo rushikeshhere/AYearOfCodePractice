3774b96892129c0b40d0740bfdc72c0dfbadb7b5
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-02 21:43:11 +0530

    Swapped two numbers using third variable

---
+++ src/com/practice/Day72/Swapping_Two_Number.java
@@ -0,0 +1,16 @@
+package com.practice.Day72;
+
+public class Swapping_Two_Number {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int temp;
+
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	}
+
+}
