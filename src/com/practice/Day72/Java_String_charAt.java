cbe2dbcacfd80c696f8e73e17ecf8812768b3d82
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-02 21:55:17 +0530

    Java String charAt()

---
+++ src/com/practice/Day72/Java_String_charAt.java
@@ -0,0 +1,9 @@
+package com.practice.Day72;
+
+public class Java_String_charAt {
+	public static void main(String[] args) {
+		String str = "Java Programming";
+		System.out.println("The character at the index of 3 is :: " + str.charAt(3));
+	}
+
+}
