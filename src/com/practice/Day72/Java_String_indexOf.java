49dbbaea9bbd21f7fe846ef97f6b73b1bb33b856
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-02 22:02:20 +0530

    java String index finder using indexOf()

---
+++ src/com/practice/Day72/Java_String_indexOf.java
@@ -0,0 +1,8 @@
+package com.practice.Day72;
+
+public class Java_String_indexOf {
+	public static void main(String[] args) {
+		String str = "Know java";
+		System.out.println("The character 'a' index is :: " + str.indexOf("a"));
+	}
+}
