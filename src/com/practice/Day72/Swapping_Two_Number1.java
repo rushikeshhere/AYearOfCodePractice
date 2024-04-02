0c914468e4292316e2bc36437eadb6c92c11957f
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-02 21:45:35 +0530

    swapped two  numbers

---
+++ src/com/practice/Day72/Swapping_Two_Number1.java
@@ -0,0 +1,14 @@
+package com.practice.Day72;
+
+public class Swapping_Two_Number1 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	}
+}
