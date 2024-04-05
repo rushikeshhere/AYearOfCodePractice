61ff9da7844e2888d64cbf343abbfe2e321226dd
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-05 22:11:47 +0530

    Total and Average of all the fields

---
+++ src/com/practice/Day73/Total_And_Average.java
@@ -0,0 +1,16 @@
+package com.practice.Day73;
+
+public class Total_And_Average {
+	public static void main(String[] args) {
+		
+		int a=65;
+		int b=78;
+		int c=90;
+		int d=50;
+		int total=a+b+c+d;
+		System.out.println("Total of all the fields :: "+total);
+		double avg=total/4;
+		System.out.println("Average of all the fields :: "+avg);
+	}
+
+}
