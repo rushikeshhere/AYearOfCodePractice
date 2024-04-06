78e8c5d49a8564845f33be04610b10b3def65960
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-06 22:43:38 +0530

    Sorted array

---
+++ src/com/practice/Day74/Sorting_Array.java
@@ -0,0 +1,11 @@
+package com.practice.Day74;
+
+import java.util.Arrays;
+
+public class Sorting_Array {
+	public static void main(String[] args) {
+		int [] arr= {50,78,12,8,65,45};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+}
