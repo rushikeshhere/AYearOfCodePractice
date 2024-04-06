65bd4d1e9e190543b47aafef358d547acb2ef688
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-06 22:38:56 +0530

    Printed Multiplication Table

---
+++ src/com/practice/Day74/Printing_Multiplication_Table.java
@@ -0,0 +1,10 @@
+package com.practice.Day74;
+
+public class Printing_Multiplication_Table {
+	public static void main(String[] args) {
+		int number = 5;
+		for (int i = 1; i <= 10; i++) {
+			System.out.printf("%d * %d =%d\n", 5, i, number * i);
+		}
+	}
+}
