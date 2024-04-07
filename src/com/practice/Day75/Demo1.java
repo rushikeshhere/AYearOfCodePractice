f628b2b104ad5e475d5f2241dc2b393065b3d321
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-07 22:42:47 +0530

    Multiplication and String

---
+++ src/com/practice/Day75/Demo1.java
@@ -0,0 +1,8 @@
+package com.practice.Day75;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("java" + 10 * 20);
+		System.out.println(10 * 20 + "java");
+	}
+}
