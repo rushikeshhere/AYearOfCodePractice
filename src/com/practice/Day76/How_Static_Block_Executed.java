de9732df86b4d5dcc8f61513f2dbc06f27b058ed
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-08 22:36:21 +0530

    How static block load when class is loaded

---
+++ src/com/practice/Day76/How_Static_Block_Executed.java
@@ -0,0 +1,12 @@
+package com.practice.Day76;
+
+public class How_Static_Block_Executed {
+	static {
+		System.out.println("Static block Invoked!");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
