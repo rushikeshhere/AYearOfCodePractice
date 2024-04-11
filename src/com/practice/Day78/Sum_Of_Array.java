e4b17bb43d73ce940efad5d3e132683d85375241
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-11 22:57:54 +0530

    sum of Array

---
+++ src/com/practice/Day78/Sum_Of_Array.java
@@ -0,0 +1,12 @@
+package com.practice.Day78;
+
+public class Sum_Of_Array {
+	public static void main(String[] args) {
+		int arr[]= {10,20,30,40,50};
+		int sum=0;
+		for(int i:arr) {
+		 sum=sum+i;	
+		}
+		System.out.println(sum);
+	}
+}
