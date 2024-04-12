da94b561e3b3e39fadaa15f43ded029971d5ec6a
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-12 23:02:36 +0530

    Updation

--- src/com/practice/Day79/SerializeStudent.java
+++ src/com/practice/Day79/SerializeStudent.java
@@ -1,32 +1,22 @@
 package com.practice.Day79;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class SerializeStudent implements Serializable {
-	int id;
-	String name;
-	String address;
-	
-	
-	public SerializeStudent(int id, String name, String address) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.address = address;
-	}
-
 
 	public static void main(String[] args) {
-		
-		SerializeStudent s= new SerializeStudent(15,"Rushikesh","dkd");
+
+		Student st = new Student(15, "Rushikesh", "dkd");
 		try {
-			FileOutputStream fis= new FileOutputStream("F:\\file.txt");
-			ObjectOutputStream oos= new ObjectOutputStream(fis);
-			oos.writeObject(s);
+			FileOutputStream fis = new FileOutputStream("F:\\file1.txt");
+			ObjectOutputStream oos = new ObjectOutputStream(fis);
+			oos.writeObject(st);
 			System.out.println("Success");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -35,7 +25,7 @@ public class SerializeStudent implements Serializable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
