858b9e40a58fd500cc86ad8ac7f11389ae754df0
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-16 22:25:32 +0530

    Static array Example

---
+++ src/com/practice/Day81/Static_Array_Test.java
@@ -0,0 +1,11 @@
+package com.practice.Day81;
+
+public class Static_Array_Test {
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5,6};
+		System.out.println(arr.length);
+		arr[6]=7;
+		System.out.println(arr.length);
+	}
+
+}
