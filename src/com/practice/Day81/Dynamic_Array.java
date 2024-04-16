4a023c050d74eb7a90d6c8c35fd05538df6173cf
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-16 22:35:22 +0530

    Dynamic Array and Sorted ArrayList

---
+++ src/com/practice/Day81/Dynamic_Array.java
@@ -0,0 +1,16 @@
+package com.practice.Day81;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Dynamic_Array {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(12);
+		list.add(75);
+		list.add(18);
+		System.out.println(list);
+		Collections.sort(list);
+		System.out.println(list);
+	}
+}
