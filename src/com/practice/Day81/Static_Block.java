0c24558dd04092b06960833f211ef8af89f9bebb
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-16 21:42:40 +0530

    static block initialization

---
+++ src/com/practice/Day81/Static_Block.java
@@ -0,0 +1,15 @@
+package com.practice.Day81;
+
+public class Static_Block {
+	static {
+		System.out.println("hi");
+	}
+	static {
+		System.out.println("hi1");
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
