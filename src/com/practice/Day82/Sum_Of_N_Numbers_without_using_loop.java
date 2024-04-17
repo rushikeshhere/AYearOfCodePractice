87e18c3f0ab7d51e66d6a11bd2004ace545b2474
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-17 18:49:42 +0530

    sum of n Natural numbers without using loops

---
+++ src/com/practice/Day82/Sum_Of_N_Numbers_without_using_loop.java
@@ -0,0 +1,11 @@
+package com.practice.Day82;
+
+public class Sum_Of_N_Numbers_without_using_loop {
+	public static void main(String[] args) {
+		int sum = 0;
+		int n = 10;
+		sum = n * (n + 1) / 2;
+		System.out.println("Sum of N numbers :: " + sum);
+	}
+
+}
