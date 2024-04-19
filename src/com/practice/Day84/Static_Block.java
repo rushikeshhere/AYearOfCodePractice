00f50f4e105288e26f3fac1891f44604a1112b5b
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-19 22:01:56 +0530

    How static block is executed before the main method

---
+++ src/com/practice/Day84/Static_Block.java
@@ -0,0 +1,12 @@
+package com.practice.Day84;
+
+public class Static_Block {
+
+	static {
+		System.out.println("Hello-world!");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hey");
+	}
+}
