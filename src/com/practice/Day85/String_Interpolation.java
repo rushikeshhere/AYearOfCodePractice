ceffa93674136620111f7c8ad38f90daaacc157f
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-20 22:57:01 +0530

    String interpolation

---
+++ src/com/practice/Day85/String_Interpolation.java
@@ -0,0 +1,14 @@
+package com.practice.Day85;
+
+public class String_Interpolation {
+	public static void main(String[] args) {
+		String name = "Harry";
+		String school = "St. Jhon's Convent";
+		String address = "100 Church Street";
+		String city = "Chicago";
+		String pinCode = "60629";
+		System.out.println("Student Details:");
+		System.out.println("Student Name: " + name + ", School: " + school + ", Address: " + address + ", City: " + city
+				+ ", PinCode: " + pinCode);
+	}
+}
