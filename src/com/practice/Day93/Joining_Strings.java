67d4bfe6e120f728a8f291ae78ed025739580524
Rushikesh Here <rushikeshhere@gmail.com>
2024-04-30 22:39:28 +0530

    Joined List Of String into the String

---
+++ src/com/practice/Day93/Joining_Strings.java
@@ -0,0 +1,19 @@
+package com.practice.Day93;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Joining_Strings {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		list.add("Pro");
+		list.add("gram");
+		list.add("ming");
+		System.out.println("List: " + list);
+		String str = " ";
+		for (String str1 : list) {
+			str = str.concat(str1);
+		}
+		System.out.println("String :: " + str);
+	}
+}
