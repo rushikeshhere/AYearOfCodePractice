e2463ce4efcdbe39fb307dcc17bd9a3fbccd7038
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-02 22:31:40 +0530

    Converted String to Integer

---
+++ src/com/practice/Day94/Converting_String_To_Integer.java
@@ -0,0 +1,9 @@
+package com.practice.Day94;
+
+public class Converting_String_To_Integer {
+	public static void main(String[] args) {
+		String str = "12345";
+		int afterConvert = Integer.parseInt(str);
+		System.out.println(afterConvert);
+	}
+}
