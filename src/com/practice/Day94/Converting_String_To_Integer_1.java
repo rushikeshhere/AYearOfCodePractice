ed11648d9aea19e0213a1535a10b2e9467367a38
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-02 22:35:25 +0530

    Converted String to Integer using .ValueOf method

---
+++ src/com/practice/Day94/Converting_String_To_Integer_1.java
@@ -0,0 +1,9 @@
+package com.practice.Day94;
+
+public class Converting_String_To_Integer_1 {
+	public static void main(String[] args) {
+		String str= "1245";
+		int afterConvert=Integer.valueOf(str);
+		System.out.println(afterConvert);
+	}
+}
