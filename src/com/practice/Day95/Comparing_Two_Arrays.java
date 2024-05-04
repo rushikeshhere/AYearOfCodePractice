9e560a05c2cb7cf6e59f0a8986cad8858dc20508
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-04 22:43:44 +0530

    Compared Two arrays

---
+++ src/com/practice/Day95/Comparing_Two_Arrays.java
@@ -0,0 +1,12 @@
+package com.practice.Day95;
+
+public class Comparing_Two_Arrays {
+	public static void main(String[] args) {
+		int arr1[]= {10,20,30,45,56};
+		int arr2[]= arr1;
+		if(arr1==arr2) 
+			System.out.println("Both are Equal!");
+		else
+			System.out.println("Not Equal...");
+	}
+}
