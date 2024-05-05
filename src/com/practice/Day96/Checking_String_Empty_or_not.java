dbc9b4b0b078d94e8700de4290581e3138084215
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-05 22:58:33 +0530

    Checked String is Empty or not

---
+++ src/com/practice/Day96/Checking_String_Empty_or_not.java
@@ -0,0 +1,11 @@
+package com.practice.Day96;
+
+public class Checking_String_Empty_or_not {
+	public static void main(String[] args) {
+		String str="";
+		if(str.isEmpty())
+			System.out.println("String str is Empty");
+		else
+			System.out.println("String str is not Empty.");
+	}
+}
