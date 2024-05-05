2b971538d9c68e0bee26c3e1ac191ce200f69b6b
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-05 22:45:00 +0530

    Removed Special char and added new special char

---
+++ src/com/practice/Day96/Removing_Special_char.java
@@ -0,0 +1,9 @@
+package com.practice.Day96;
+
+public class Removing_Special_char {
+	public static void main(String[] args) {
+		String str= "java@121";
+		System.out.println(str.replace('@', '$'));
+	}
+
+}
