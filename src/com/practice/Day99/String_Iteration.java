81eab492b82eb4d1ac6c8a5c6e5f43b01ab83d70
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-08 17:48:06 +0530

    String Iteration

---
+++ src/com/practice/Day99/String_Iteration.java
@@ -0,0 +1,11 @@
+package com.practice.Day99;
+
+public class String_Iteration {
+	public static void main(String[] args) {
+		String str="Rushikesh";
+		int length=str.length();
+		for(int i=0;i<length;i++) {
+			System.out.print(str.charAt(i)+" ");
+		}
+	}
+}
