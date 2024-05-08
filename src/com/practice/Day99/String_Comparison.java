1358cafd56d3431cec188751b6008177d41a66b2
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-08 17:29:28 +0530

    String comparison

---
+++ src/com/practice/Day99/String_Comparison.java
@@ -0,0 +1,10 @@
+package com.practice.Day99;
+
+public class String_Comparison {
+	public static void main(String[] args) {
+		String str1 = "hello";
+		String str2 = "world";
+		String result = str1 + str2;
+		System.out.println(result == "helloworld");
+	}
+}
