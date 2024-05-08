3e93a5910392ed619884b1c7da485fbb37794e21
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-08 17:18:12 +0530

    Found Gretest Number

---
+++ src/com/practice/Day99/GretestNumber.java
@@ -0,0 +1,11 @@
+package com.practice.Day99;
+
+public class GretestNumber {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 5;
+		int z = (x > y) ? x : y;
+		System.out.println("The Gretest Number is :: "+z);
+	}
+
+}
