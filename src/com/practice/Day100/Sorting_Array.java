c44d89e77c4463b254d17fca0ad5886d13be291b
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-09 23:29:20 +0530

    Sorted Array

---
+++ src/com/practice/Day100/Sorting_Array.java
@@ -0,0 +1,14 @@
+package com.practice.Day100;
+
+import java.util.Arrays;
+
+public class Sorting_Array {
+	public static void main(String[] args) {
+
+		int arr[] = { 3, 2, 1, 7, 4, 5, 9, 14 };
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
