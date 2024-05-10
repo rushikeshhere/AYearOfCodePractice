a8e7b997379521423d324e896714c11cda4f76f1
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-10 22:38:14 +0530

    Checked Lowercase or Uppercase

---
+++ src/com/practice/Day101/Lowercase.java
@@ -0,0 +1,11 @@
+package com.practice.Day101;
+
+public class Lowercase {
+	public static void main(String[] args) {
+		char ch = 'l';
+		if (Character.isLowerCase(ch))
+			System.out.println("LowerCase!");
+		else
+			System.out.println("Uppercase!");
+	}
+}
