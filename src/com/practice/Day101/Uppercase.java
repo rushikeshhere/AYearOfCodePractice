098221699e00d1b5af0cc4a6ae798d2344eceb98
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-10 22:36:13 +0530

    Checked Char is UpperCase or LowerCase

---
+++ src/com/practice/Day101/Uppercase.java
@@ -0,0 +1,12 @@
+package com.practice.Day101;
+
+public class Uppercase {
+	public static void main(String[] args) {
+		char ch='U';
+		if(Character.isUpperCase(ch))
+			System.out.println("Uppercase!");
+		else
+			System.out.println("lowecase!");
+	}
+
+}
