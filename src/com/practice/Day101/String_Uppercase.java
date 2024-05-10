f7cc2fc063ecfb61543cf7d3f2343a414518f879
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-10 22:41:14 +0530

    Checker String is Equal with uppercase or not

---
+++ src/com/practice/Day101/String_Uppercase.java
@@ -0,0 +1,12 @@
+package com.practice.Day101;
+
+public class String_Uppercase {
+	public static void main(String[] args) {
+		String str="Java";
+		if(str.equals(str.toUpperCase()))
+			System.out.println("True");
+		else
+			System.out.println("false");
+	}
+
+}
