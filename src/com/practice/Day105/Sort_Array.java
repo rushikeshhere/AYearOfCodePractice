24559c63ef2704770eacc7fec528d4965a3e7611
Rushikesh Here <rushikeshhere@gmail.com>
2024-05-15 22:36:04 +0530

    Sort Arrays

---
+++ src/com/practice/Day105/Sort_Array.java
@@ -0,0 +1,12 @@
+package com.practice.Day105;
+
+import java.util.Arrays;
+
+public class Sort_Array {
+	public static void main(String[] args) {
+		int array[] = { 10, 45, 78, 62, 82, 28 };
+		Arrays.sort(array);
+		System.out.println(Arrays.toString(array));
+	}
+
+}
