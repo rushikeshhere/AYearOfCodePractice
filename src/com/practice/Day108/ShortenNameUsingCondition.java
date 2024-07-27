bcd6b6339760e7f4fef4d699e9e51eb7240160e8
Rushikesh Here <rushikeshhere@gmail.com>
2024-07-27 22:44:13 +0530

    Added one comment in program

--- src/com/practice/Day108/ShortenNameUsingCondition.java
+++ src/com/practice/Day108/ShortenNameUsingCondition.java
@@ -3,14 +3,17 @@ package com.practice.Day108;
 public class ShortenNameUsingCondition {
 	public static String shortenName(String name,int maxLength) {
 		// checked name is not
+		/*if(name==null || name.length()<=maxLength)
+			return name;*/
+		// This is then one way to understand condition
 		if(name==null || name.length()<=maxLength)
-			return name;
+			return "Pass a String to truncate";
 		return 	name.substring(0, maxLength)+"....";
 	}
 	
 	
 	public static void main(String[] args) {
-		String name="Rushikesh here";
+		String name="";
 		int maxLength=7;
 		System.out.println(shortenName(name, maxLength));
 		
