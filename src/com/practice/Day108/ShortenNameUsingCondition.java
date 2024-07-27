77e3fe6b251f02ba3f4f4d3c441b3d30d84f26b6
Rushikesh Here <rushikeshhere@gmail.com>
2024-07-27 22:39:18 +0530

    Same code but added Conditional check

---
+++ src/com/practice/Day108/ShortenNameUsingCondition.java
@@ -0,0 +1,20 @@
+package com.practice.Day108;
+
+public class ShortenNameUsingCondition {
+	public static String shortenName(String name,int maxLength) {
+		// checked name is not
+		if(name==null || name.length()<=maxLength)
+			return name;
+		return 	name.substring(0, maxLength)+"....";
+	}
+	
+	
+	public static void main(String[] args) {
+		String name="Rushikesh here";
+		int maxLength=7;
+		System.out.println(shortenName(name, maxLength));
+		
+	}
+
+
+}
